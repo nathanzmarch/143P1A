3538258f1db5d50cf23a140071b2411dce3fb3f6
Kevin Liu <kevinliu732@gmail.com>
2019-01-27 17:44:45 -0800

    Add violation error messages and readme

---
+++ readme.txt
@@ -0,0 +1,22 @@
+Nathan March
+UID: 404827938
+
+Kevin Liu
+UID: 504862375
+
+CS 143
+Winter 2019
+
+===============================================================================
+
+Project 1 Part A 
+Spec: http://yellowstone.cs.ucla.edu/cs143/project/project1A.html
+
+Files:
+* create.sql
+* load.sql
+* queries.sql
+* query.php
+* violate.sql
+* team.txt
+* readme.txt
--- violate.sql
+++ violate.sql
@@ -1,12 +1,15 @@
 -- Primary Key Constraints
 INSERT INTO Movie VALUES(272, 'Primary Key Violation', 1999, 'PG', 'Constraint');
 -- Movie table already has tuple with id = 272
+-- ERROR 1062 (23000): Duplicate entry '272' for key 'PRIMARY'
 
-INSERT INTO Actor VALUES(126, 'Violation', 'Primary Key', 'Male', 1999-01-01, NULL);
--- Actor table already has tuple with id = 999
+INSERT INTO Actor VALUES(67295, 'Violation', 'Primary Key', 'Male', 1999-01-01, NULL);
+-- Actor table already has tuple with id = 67295
+-- ERROR 1062 (23000): Duplicate entry '67295' for key 'PRIMARY'
 
 INSERT INTO Director VALUES(37146, 'Violation', 'Primary Key', 1999-01-01, NULL);
--- Director table already has tuple with id = 974
+-- Director table already has tuple with id = 37146
+-- ERROR 1062 (23000): Duplicate entry '37146' for key 'PRIMARY'
 
 
 -- Check Constraints
@@ -26,21 +29,28 @@ INSERT INTO MovieRating VALUES(272, 89, 0);
 -- Foreign Key Constraints
 INSERT INTO Sales VALUES(999, 20, 1000000);
 -- No tuple exists in table Movie with id = 999
+-- ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`Sales`, CONSTRAINT `Sales_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`))
 
 INSERT INTO MovieGenre VALUES(999, 'Drama');
 -- No tuple exists in table Movie with id = 999
+-- ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieGenre`, CONSTRAINT `MovieGenre_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`))
 
 INSERT INTO MovieDirector VALUES(999, 37146);
 -- No tuple exists in table Movie with id = 999
+-- ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieDirector`, CONSTRAINT `MovieDirector_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`))
 
 INSERT INTO MovieDirector VALUES(272, 1000);
 -- No tuple exists in table Director with id = 1000
+-- ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieDirector`, CONSTRAINT `MovieDirector_ibfk_2` FOREIGN KEY (`did`) REFERENCES `Director` (`id`))
 
 INSERT INTO MovieActor VALUES(999, 1, 'Foreign Key Violator');
 -- No tuple exists in table Movie with id = 999
+-- ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieActor`, CONSTRAINT `MovieActor_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`))
 
 INSERT INTO MovieActor VALUES(272, 20010, 'Foreign Key Violator');
 -- No tuple exists in table Actor with id = 20010
+-- ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieActor`, CONSTRAINT `MovieActor_ibfk_2` FOREIGN KEY (`aid`) REFERENCES `Actor` (`id`))
 
 INSERT INTO MovieRating VALUES(999, 401, 50);
 -- No tuple exists in table Movie with id = 999
+-- ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieRating`, CONSTRAINT `MovieRating_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`))
